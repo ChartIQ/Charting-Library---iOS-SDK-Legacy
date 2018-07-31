2d9a896c0bd7d9d24dd1b47c4456fc9b8f44b47e
Natsios Konstantinos <kwstasna@ATH-SDV-MC20-FI.local>
2018-07-31 15:34:34 +0300

    changes in set decimal places functionality

--- ChartIQ/Charts/ChartIQView.swift
+++ ChartIQ/Charts/ChartIQView.swift
@@ -954,7 +954,7 @@ public class ChartIQView: UIView {
     /// - Parameters:
     ///   - decimalPlaces: number of decimal places
     public func setDecimalPlaces(_ decimalPlaces: Int) {
-        let script = "stxx.chart.yAxis.decimalPlaces=\(decimalPlaces)"
+        let script = "setDecimalPlacesWith(\(decimalPlaces));"
         webView.evaluateJavaScript(script, completionHandler: nil)
     }
     
@@ -1293,7 +1293,7 @@ public class ChartIQView: UIView {
         var addedStudy = [Study]()
         let script = "getAddedStudies();"
         if let listString = webView.evaluateJavaScriptWithReturn(script), !listString.isEmpty {
-            let list = listString.components(separatedBy: "|||") // changed from two (2) separators to three (3) separators because the join in js file is with three (3) separators, and some studies could not fetch their parameters.
+            let list = listString.components(separatedBy: "||")
             list.forEach({ (study) in
                 let components = study.components(separatedBy: "___")
                 var name = components[0]
