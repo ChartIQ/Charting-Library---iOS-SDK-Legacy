a7beb5ef32eba7da0470782f01cabd9d73cfcd35
Natsios Konstantinos <kwstasna@ATH-SDV-MC20-FI.local>
2018-08-02 15:49:29 +0300

    deleted function not needed

--- ChartIQ/Charts/ChartIQView.swift
+++ ChartIQ/Charts/ChartIQView.swift
@@ -953,15 +953,6 @@ public class ChartIQView: UIView {
     ///
     /// - Parameters:
     ///   - decimalPlaces: number of decimal places
-    public func xm_setDecimalPlaces(_ decimalPlaces: Int) {
-        let script = "setDecimalPlacesWith(\(decimalPlaces));"
-        webView.evaluateJavaScript(script, completionHandler: nil)
-    }
-    
-    /// Set decimal places
-    ///
-    /// - Parameters:
-    ///   - decimalPlaces: number of decimal places
     public func setDecimalPlaces(_ decimalPlaces: Int) {
         let script = "stxx.chart.yAxis.decimalPlaces=\(decimalPlaces);"
         webView.evaluateJavaScript(script, completionHandler: nil)
